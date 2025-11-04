e33d26d8798023d54b0275173f105e5841b6c5dd
kaBenitez <kevinescudoderoble@gmail.com>
2025-11-04 19:35:04 -0300

    better html

--- Servers/Server1/index.js
+++ Servers/Server1/index.js
@@ -2,7 +2,38 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hola desde Servidor 1 ');
+
+const html= 
+  `<html>
+      <head>
+        <title>Servidor 1</title>
+        <style>
+          body { 
+            font-family: Arial, sans-serif; 
+            background-color: #282c34; 
+            color: white; 
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+          }
+          h1 {
+            color: #61dafb; /* Un color azul tipo React */
+            font-size: 3rem;
+            text-shadow: 2px 2px 4px #000000;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Hola desde el servidor 1</h1>
+      </body>
+    </html>
+  `;
+
+
+
+  res.send(html);
 });
 
 app.listen(3000, () => {
